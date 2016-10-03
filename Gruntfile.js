282c36964dd81b0dd8ba62121535d818a351c2c2
Kyle Sullivan <kylepsullivan@gmail.com>
2016-10-03 06:52:34 -0400

    Optimize images

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,6 +14,16 @@ module.exports = function (grunt) {
                 }
             }
         },
+        imagemin: {                          // Task
+          dynamic: {                         // Another target
+            files: [{
+              expand: true,                  // Enable dynamic expansion
+              cwd: 'images/',                   // Src matches are relative to this path
+              src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+              dest: 'images/'                  // Destination path prefix
+            }]
+          }
+        },
         watch: {
             styles: {
                 files: ['sass/*.scss'],
@@ -24,6 +34,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-autoprefixer');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-    grunt.registerTask('default', ['sass', 'autoprefixer']);
+    grunt.registerTask('default', ['sass', 'autoprefixer', 'imagemin']);
 };
